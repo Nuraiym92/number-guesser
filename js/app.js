60389e194755a47678f2964a7c3bd91ee6b80d1f
Nuraiym <nuraiym.zhyrgalbekova.kk@gmail.com>
2022-02-21 22:28:57 +0600

    added number guesser game

---
+++ js/app.js
@@ -0,0 +1,68 @@
+let min=1;
+let max=10;
+let winingNum=5;
+
+const btn=document.querySelector("#guess-btn");
+const message=document.querySelector(".message");
+const maxNum=document.querySelector(".max-num");
+const minNum=document.querySelector(".min-num");
+const guessInput=document.querySelector("#guess-input");
+
+ let attempt=document.querySelector(".attempt")
+
+minNum.textContent=min;
+maxNum.textContent=max;
+
+
+
+
+btn.addEventListener("click",function(){
+
+let guess=parseInt(guessInput.value);
+
+if(isNaN(guess)||guess<min||guess>max){
+setMessage(`Нужно ввести число от ${min} до ${max},"red`)
+}
+
+if (guess===winingNum){
+    
+    guessInput.style.border="1px solid green";
+    setMessage(`Поздравляем ! Вы угадали ${winingNum}`,"green");
+    guessInput.value="";
+    btn.textContent="Новая игра";
+    btn.addEventListener("click",newGame)
+  
+    let numAttempt=parseInt(attempt.textContent)
+    numAttempt=numAttempt-1;
+    
+let a=numAttempt.toString();
+
+attempt.textContent=a;
+ 
+}else{
+    guessInput.style.border="1px solid red";
+    guessInput.value="";
+    setMessage(`Извините вы проиграли`,"red")
+  
+    let numAttempt=parseInt(attempt.textContent)
+    numAttempt=numAttempt-1;
+    
+let a=numAttempt.toString();
+
+attempt.textContent=a;
+
+}
+
+});
+
+function setMessage(msg,color){
+    message.textContent=msg;
+    message.style.color=color;
+}
+
+function newGame(){
+    location.reload();
+}
+
+
+
